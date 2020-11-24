3116e80407f8b03c48e672cc3946924abe270b1e
LuGew <lugew@outlook.com>
2020-11-24 18:07:57 +0800

    init

---
+++ ppt/src/test/java/com/lugew/winsimcloud/ppt/PptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugew.winsimcloud.ppt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
